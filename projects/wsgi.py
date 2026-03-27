141025efb8a27f453a91267d96e477e7bcbd14c5
Hector Uribe <dgic.h_uribe@juarez.gob.mx>
2026-03-27 12:31:50 -0600

    Fix problema utf

--- projects/wsgi.py
+++ projects/wsgi.py
@@ -1,20 +1,4 @@
-"""
-WSGI config for projects project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-# -*- coding: utf-8 -*-
-
 import os
-# --- INICIO DE SOLUCIÓN DE CODIFICACIÓN ---
-import locale
-# FUERZA LA CONFIGURACIÓN REGIONAL A UTF-8 antes de cargar Django
-locale.setlocale(locale.LC_ALL, 'en_US.UTF-8') 
-# --- FIN DE SOLUCIÓN DE CODIFICACIÓN ---
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projects.settings')
